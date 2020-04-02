846d6a81050204c587d010349eddc6c83db2ef43
godonan <godonanabt@gmail.cm>
2020-04-02 11:10:24 -0500

    VAE Minor update

---
+++ src/VAE_MNIST.py
@@ -0,0 +1,8 @@
+# Import necessary modules
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import tensorflow as tf
+
