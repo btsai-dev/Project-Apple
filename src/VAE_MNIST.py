6c3e33bac7cf9a89d2c17a07c468e2ae5f76dc81
godonan <godonanabt@gmail.cm>
2020-04-02 11:12:10 -0500

    VAE Clear

--- src/VAE_MNIST.py
+++
@@ -1,8 +0,0 @@
-# Import necessary modules
-from __future__ import division, print_function, absolute_import
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
-import tensorflow as tf
-
